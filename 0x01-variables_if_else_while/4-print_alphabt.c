ce173f65645d3de217971a5a53db18244ff23e90
Alvin <waiganjokibet001@gmail.com>
2023-08-11 12:29:29 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,15 @@
 int main(void)
 {
 	char c;
+	c = 'a';
 
 	while (c <= 'z')
+	{
 		if ((c != 'q' && c != 'e') && c <= 'z')
-		{
+
 			putchar(c);
-			c++;
-		}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print single digit numbers.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+		printf("@d", a);
+	putchar('\n');
+	return (0);
+}
