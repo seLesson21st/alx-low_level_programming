36867b436b10ec996a60a9935ceed67b50a99f5d
Alvin <waiganjokibet001@gmail.com>
2023-08-11 13:56:37 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; a < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
@@ -18,6 +18,6 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
