13231b9d6c2f0f38ec029bded898fe2462b99554
Alvin <waiganjokibet001@gmail.com>
2023-08-15 06:05:42 -0700

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  *print_sign - Print the sign of a number
  *@i: Number to be checked
- *Return: 1 for positive number, -1 for negative number or zero for anything else
+ *Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int i)
