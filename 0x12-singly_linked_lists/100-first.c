71a37030e0af040939453f000b3a9ae7cfda4451
seLesson21st <waiganjokibet001@gmail.com>
2023-09-21 13:39:08 -0700

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first_c function that prints a sentence before main function
+ */
+void first_c(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
