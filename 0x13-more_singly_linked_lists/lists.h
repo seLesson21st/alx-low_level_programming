a800f4113a72668b7906725293a564a9dd2e2ca3
seLesson21st <waiganjokibet001@gmail.com>
2023-09-25 22:57:01 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef lists_h
 #define lists_h
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -32,7 +37,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#include<stdio.h>
-#include<stdlib.h>
 
 #endif
