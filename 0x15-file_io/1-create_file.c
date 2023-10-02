156ac24447db991e9756b0aaa3ef402a6ffa2831
seLesson21st <waiganjokibet001@gmail.com>
2023-10-02 11:34:30 -0700

    reviewed command

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file_x = open(filename, O_CREAT | O_WRONLY, 0600);
+	file_x = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_x == -1)
 	{
 		return (-1);
